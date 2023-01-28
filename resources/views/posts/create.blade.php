8e9f62be22ed5f0727028741474a7b794ec84f95
edriso <edriso.dev@gmail.com>
2023-01-28 18:41:02 +0200

    complete posts.create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,1 +1,28 @@
-<h2>create</h2>
+@extends('layouts.app')
+
+@section('title')
+    Create New Post
+@endsection
+
+@section('content')
+<form action="/posts" method="POST">
+    @csrf
+  <div class="form-group mb-3">
+    <label for="post-title">Title</label>
+    <input type="text" class="form-control" id="post-title" placeholder="Post Title">
+  </div>
+  <div class="form-group mb-3">
+    <label for="post-content">Content</label>
+    <textarea class="form-control" id="post-content" rows="3"></textarea>
+  </div>
+  <div class="form-group mb-4">
+    <label for="post-author">Author</label>
+    <select class="form-control" id="post-author">
+      <option>John Doe</option>
+      <option>Jane Doe</option>
+    </select>
+  </div>
+  
+  <button type="submit" class="btn btn-success px-4 py-2">Post</button>
+</form>
+@endsection
