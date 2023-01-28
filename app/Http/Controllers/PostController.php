567f823dd2b3e67b6a53df2fe039457ec3a43d37
edriso <edriso.dev@gmail.com>
2023-01-28 18:59:35 +0200

    complete posts.show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,8 +42,21 @@ class PostController extends Controller
     }
 
     public function show($postId) {
-        // dd($postId);
-        return view('posts.show');
+        $selectedPost = [
+                'id' => 1,
+                'title' => 'Cupcake Ipsum!',
+                'content' => 'Cupcake wafer jelly-o chocolate cake pie chocolate lollipop. Pastry bonbon bonbon brownie apple pie dragée. Croissant I love pastry apple pie macaroon sesame snaps cake. Jujubes I love pudding biscuit marzipan tootsie roll.
+                            Donut oat cake I love cupcake croissant. Halvah sesame snaps pie gummies pie lollipop I love sweet. Toffee oat cake halvah oat cake topping powder lollipop.
+                            Toffee chocolate bar powder chupa chups jelly donut. Topping I love candy canes bonbon chocolate bar. Bonbon sweet roll brownie I love tiramisu topping halvah gingerbread tootsie roll.
+                            Caramels donut tiramisu biscuit sweet jujubes. Cupcake pastry topping macaroon I love I love. Oat cake fruitcake wafer icing sweet I love danish shortbread.
+                            Carrot cake gummi bears pastry cookie tootsie roll bear claw bonbon. Danish jujubes sugar plum donut I love I love jelly bonbon. Soufflé sugar plum toffee marshmallow ice cream. Macaroon cake cheesecake marzipan cheesecake soufflé chocolate bar tootsie roll.',
+                'posted_by' => 'John Doe',
+                'created_at' => '2022-01-25 05:00:00'
+        ];
+
+        return view('posts.show', [
+            'post' => $selectedPost
+        ]);
     }
 
     public function edit($postId) {
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,1 +1,22 @@
-<h2>show</h2>
+@extends('layouts/app')
+
+@section('title')
+    Post Details
+@endsection
+
+@section('content')
+{{-- @dd($post) --}}
+<div class="card text-center">
+  <div class="card-header">
+    {{'@'.$post['posted_by']}}
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">{{$post['title']}}</h5>
+    <p class="card-text">{{$post['content']}}</p>
+    {{-- <a href="#" class="btn btn-primary btn-sm">Go somewhere</a> --}}
+  </div>
+  <div class="card-footer text-muted">
+    {{$post['created_at']}}
+  </div>
+</div>
+@endsection
