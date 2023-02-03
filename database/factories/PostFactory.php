3db4d8bf0a1809e1cf3d561f2631dc8e64c1914f
edriso <edriso.dev@gmail.com>
2023-02-03 17:46:20 +0200

    add PostSeeder, PostFactory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $faker = fake('en_US');
+
+        return [
+            'title' => $faker->text(35),
+            'content' => $faker->realTextBetween(10, 100),
+            'user_id' => $faker->numberBetween(1, 4),
+        ];
+    }
+}
